83a9a28cab32e497f2671f514107accc1390208a
tinchodias <tinchodias@gmail.com>
2016-12-11 15:39:37 -0300

    angularLazyImg

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('app', ['ui.router', 'wu.masonry', 'facebook']);
+var app = angular.module('app', ['ui.router', 'wu.masonry', 'facebook', 'angularLazyImg']);
 
 
 
